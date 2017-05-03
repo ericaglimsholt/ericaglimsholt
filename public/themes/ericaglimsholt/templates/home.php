4fd202a537d7166ce58ebaca4fdf67a87800f5c6
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-05-03 15:24:25 +0200

    Added a header

--- public/themes/ericaglimsholt/templates/home.php
+++ public/themes/ericaglimsholt/templates/home.php
@@ -6,4 +6,19 @@ Template Name: Home
 
 <?php get_header(); ?>
 
+</div>
+
+<div class="home">
+    <div class="container">
+        <div class="col-md-7">
+            <div class="content">
+                <h1>Hello, I'm Erica <span class="blinking-cursor">|</span></h1>
+                <hr>
+                <p>I'm a freelancer who combines design and web development to create the perfect, customized digital solution for you.</p>
+            </div>
+        </div>
+    </div>
+
+</div>
+
 <?php get_footer(); ?>
