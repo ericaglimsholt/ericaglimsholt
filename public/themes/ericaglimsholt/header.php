eb513ab65d4916ba26fdc461d49bc4522ec2a8f0
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-05-03 18:50:31 +0200

    Created a about page

--- public/themes/ericaglimsholt/header.php
+++ public/themes/ericaglimsholt/header.php
@@ -11,6 +11,7 @@
 
         <!-- Google fonts-->
         <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Inconsolata:400,700" rel="stylesheet">
 
         <!-- Bootstrap CSS -->
        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
--- public/themes/ericaglimsholt/templates/home.php
+++ public/themes/ericaglimsholt/templates/home.php
@@ -12,13 +12,12 @@ Template Name: Home
     <div class="container">
         <div class="col-md-7">
             <div class="content">
-                <h1>Hello, I'm Erica <span class="blinking-cursor">|</span></h1>
+                <h1><?php the_field('header_title'); ?><span class="blinking-cursor">|</span></h1>
                 <hr>
-                <p>I'm a freelancer who combines design and web development to create the perfect, customized digital solution for you.</p>
+                <p><?php the_field('header_description'); ?></p>
             </div>
         </div>
     </div>
-
 </div>
 
 <?php get_footer(); ?>
