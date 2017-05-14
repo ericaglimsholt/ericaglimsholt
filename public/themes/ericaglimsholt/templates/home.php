3f50637667847cc6dc8c2c9ca7bde66f633256aa
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-05-14 20:32:46 +0200

    Added icons

--- public/themes/ericaglimsholt/templates/home.php
+++ public/themes/ericaglimsholt/templates/home.php
@@ -19,5 +19,20 @@ Template Name: Home
         </div>
     </div>
 </div>
+<div class="container">
+
+    <div class="row icons">
+        <?php if( have_rows('icons') ): ?>
+            <?php while ( have_rows('icons') ) : the_row(); ?>
+                <div class="col-md-4">
+                    <div class="icons">
+                        <img src="<?php the_sub_field('image'); ?>" alt="">
+                        <h4><?php the_sub_field('title'); ?></h4>
+                        <?php the_sub_field('description'); ?>
+                    </div>
+                </div>
+            <?php endwhile; ?>
+        <?php endif; ?>
+    </div>
 
 <?php get_footer(); ?>
