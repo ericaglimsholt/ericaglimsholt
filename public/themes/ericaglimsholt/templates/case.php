ed7cdf88defd1288cb95a3020e89a354d0774c41
Erica Glimsholt <ericaglimsholt@hotmail.com>
2017-05-14 19:57:10 +0200

    Added cases

--- public/themes/ericaglimsholt/templates/case.php
+++ public/themes/ericaglimsholt/templates/case.php
@@ -26,15 +26,35 @@ Template Name: Case
             <p><?php the_field('description'); ?></p>
         </div>
     </div>
-    <div class="row case">
-        <div class="col-md-8">
-            <?php if( have_rows('companies') ): ?>
-                <?php while ( have_rows('companies') ) : the_row(); ?>
-                    <h2><?php the_sub_field('column_1'); ?></h2>
+
+    <div class="row img">
+        <div class="col-md-6">
+            <?php if( have_rows('work') ): ?>
+                <?php while ( have_rows('work') ) : the_row(); ?>
+                    <div class="work">
+                        <a href="<?php the_sub_field('url'); ?>">
+                            <img src="<?php the_sub_field('image'); ?>" alt="">
+                        </a>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </div>
+        <div class="col-md-6">
+            <?php if( have_rows('work_copy') ): ?>
+                <?php while ( have_rows('work_copy') ) : the_row(); ?>
+                    <div class="work">
+                        <a href="<?php the_sub_field('url'); ?>">
+                            <img src="<?php the_sub_field('image'); ?>" alt="">
+                        </a>
+                    </div>
                 <?php endwhile; ?>
             <?php endif; ?>
         </div>
     </div>
+
+
+
+
 
 
 
